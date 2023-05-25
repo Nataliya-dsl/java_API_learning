3b31c1725c6be8c4fe46f33ad0c6ff381dd63cd7
Nataliya-dsl <73291079+Nataliya-dsl@users.noreply.github.com>
2023-05-25 13:39:17 +0200

    Add params

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,13 +1,20 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HelloWorldTest {
 
     @Test
-    public void testHelloWorld() {
+    public void testRestAssured() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "Natali");
+
         Response response = RestAssured
+                .given()
+                .queryParams(params)
                 .get("https://playground.learnqa.ru/api/hello")
                 .andReturn();
         response.prettyPrint();
